9d25cec78565207a916bf796a58813ff61bc088f
daga955 <j.okarkas.m45+k42mkp1a6j@gmail.com>
2025-06-19 09:49:40 +0100

    Added package-lock.json

---
+++ main.js
@@ -0,0 +1,10 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>Welcome to Tripped!</h1>
+    <p class="read-the-docs">
+      Your project is now set up and ready to go.
+    </p>
+  </div>
+`
